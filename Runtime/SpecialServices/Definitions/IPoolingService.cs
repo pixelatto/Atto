bbb642d52e5b171186b2080a033121dbf0c1d6d5
javicepa <javicepa@gmail.com>
2019-04-18 11:25:44 +0200

    Add pooling service and provider

---
+++ Runtime/SpecialServices/Definitions/IPoolingService.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IPoolingService
+{
+
+    GameObject Spawn(GameObject prefab, Vector3 position, Quaternion rotation);
+    void Despawn(GameObject target);
+
+}
