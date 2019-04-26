dec7aef3ccd8f3b5341b7bc678b7db6eae487383
javicepa <javicepa@gmail.com>
2019-04-26 16:14:55 +0200

    Prepare for caching

--- Runtime/BindServiceAttribute.cs
+++ Runtime/BindServiceAttribute.cs
@@ -8,13 +8,16 @@ public class BindService : Attribute
 
     public string accessDescriptor = "";
     public ServiceMode serviceMode;
+    public ServiceCaching serviceCaching;
 
-    public BindService(string accessDescriptor, ServiceMode serviceMode = ServiceMode.Enabled)
+    public BindService(string accessDescriptor, ServiceMode serviceMode = ServiceMode.Enabled, ServiceCaching serviceCaching = ServiceCaching.Static)
     {
         this.accessDescriptor = accessDescriptor;
         this.serviceMode = serviceMode;
+        this.serviceCaching = serviceCaching;
     }
 
 }
 
 public enum ServiceMode { Undefined, Enabled, Hidden, Disabled }
+public enum ServiceCaching { Undefined, Static, Dynamic }
