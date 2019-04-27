3659d3550077a41afd8668524affe5aefd4ab17d
Javi Cepa <javicepa@gmail.com>
2019-04-27 02:23:34 +0200

    Add sound service

---
+++ Runtime/CommonServices/Definitions/ISoundService.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface ISoundService
+{
+
+    void PlaySound(AudioClip clip, PlaybackOptions options = null);
+    void ChangeMusic(AudioClip clip, PlaybackOptions options = null);
+    void FadeOutMusic(float fadeTime);
+    void FadeInMusic(float fadeTime);
+
+}
+
+public class PlaybackOptions
+{
+    public float volume = -1;
+    public float pitch = -1;
+}
