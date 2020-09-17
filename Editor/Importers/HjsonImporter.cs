4fc947990ac98f64c02974636d20832e942497a9
JaviCepa <javi@pixelatto.com>
2020-09-17 18:57:29 +0200

    Fix missing editor reference when building

--- Editor/Importers/HjsonImporter.cs
+++ Editor/Importers/HjsonImporter.cs
@@ -3,13 +3,15 @@ using UnityEditor;
 
 using System.IO;
 
-[UnityEditor.Experimental.AssetImporters.ScriptedImporter(1, "hjson")]
-public class HjsonImporter : UnityEditor.Experimental.AssetImporters.ScriptedImporter
+#if UNITY_EDITOR
+[UnityEditor.AssetImporters.ScriptedImporter(1, "hjson")]
+public class HjsonImporter : UnityEditor.AssetImporters.ScriptedImporter
 {
-	public override void OnImportAsset(UnityEditor.Experimental.AssetImporters.AssetImportContext context)
+	public override void OnImportAsset(UnityEditor.AssetImporters.AssetImportContext context)
 	{
 		TextAsset subAsset = new TextAsset(File.ReadAllText(context.assetPath));
 		context.AddObjectToAsset("text", subAsset);
 		context.SetMainObject(subAsset);
 	}
 }
+#endif
