a51df3612ff1a33edda74db00dd31c97a7dfa901
Javi Cepa <javicepa@gmail.com>
2024-05-10 11:50:43 +0200

    Improve draw tools

--- Runtime/Scripts/Tools/Draw.cs
+++ Runtime/Scripts/Tools/Draw.cs
@@ -1,29 +1,32 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 public static class Draw
 {
-    public static void Rect(this RectInt rect, Color color)
+    public static void Rect(this RectInt rect, Color? color = null)
     {
         Rect(rect.ToRect(1), color);
     }
 
-    public static void Rect(this Rect rect, Color color)
+    public static void Rect(this Rect rect, Color? color = null)
     {
-        Gizmos.color = color;
+        if (color == null) { color = Color.white; }
 
         Vector3 topLeft = new Vector3(rect.xMin, rect.yMin, 0);
         Vector3 topRight = new Vector3(rect.xMax, rect.yMin, 0);
         Vector3 bottomLeft = new Vector3(rect.xMin, rect.yMax, 0);
         Vector3 bottomRight = new Vector3(rect.xMax, rect.yMax, 0);
 
-        Debug.DrawLine(topLeft, topRight, color);
-        Debug.DrawLine(topRight, bottomRight, color);
-        Debug.DrawLine(bottomRight, bottomLeft, color);
-        Debug.DrawLine(bottomLeft, topLeft, color);
+        Debug.DrawLine(topLeft, topRight, (Color)color);
+        Debug.DrawLine(topRight, bottomRight, (Color)color);
+        Debug.DrawLine(bottomRight, bottomLeft, (Color)color);
+        Debug.DrawLine(bottomLeft, topLeft, (Color)color);
     }
 
-    public static void WireBox(Vector3 center, Vector3 halfExtents, Color color)
+    public static void WireBox(Vector3 center, Vector3 halfExtents, Color? color = null)
     {
+        if (color == null) { color = Color.white; }
+
         var v1 = center + new Vector3(+halfExtents.x, +halfExtents.y, +halfExtents.z);
         var v2 = center + new Vector3(-halfExtents.x, +halfExtents.y, +halfExtents.z);
         var v3 = center + new Vector3(-halfExtents.x, +halfExtents.y, -halfExtents.z);
@@ -33,24 +36,25 @@ public static class Draw
         var v7 = center + new Vector3(-halfExtents.x, -halfExtents.y, -halfExtents.z);
         var v8 = center + new Vector3(+halfExtents.x, -halfExtents.y, -halfExtents.z);
 
-        Debug.DrawLine(v1, v2, color);
-        Debug.DrawLine(v2, v3, color);
-        Debug.DrawLine(v3, v4, color);
-        Debug.DrawLine(v4, v1, color);
+        Debug.DrawLine(v1, v2, (Color)color);
+        Debug.DrawLine(v2, v3, (Color)color);
+        Debug.DrawLine(v3, v4, (Color)color);
+        Debug.DrawLine(v4, v1, (Color)color);
 
-        Debug.DrawLine(v5, v6, color);
-        Debug.DrawLine(v6, v7, color);
-        Debug.DrawLine(v7, v8, color);
-        Debug.DrawLine(v8, v5, color);
+        Debug.DrawLine(v5, v6, (Color)color);
+        Debug.DrawLine(v6, v7, (Color)color);
+        Debug.DrawLine(v7, v8, (Color)color);
+        Debug.DrawLine(v8, v5, (Color)color);
 
-        Debug.DrawLine(v1, v5, color);
-        Debug.DrawLine(v2, v6, color);
-        Debug.DrawLine(v3, v7, color);
-        Debug.DrawLine(v4, v8, color);
+        Debug.DrawLine(v1, v5, (Color)color);
+        Debug.DrawLine(v2, v6, (Color)color);
+        Debug.DrawLine(v3, v7, (Color)color);
+        Debug.DrawLine(v4, v8, (Color)color);
     }
 
-    public static void Circle(Vector2 center, float radius, Color color, int resolution = 20)
+    public static void Circle(Vector2 center, float radius, Color? color = null, int resolution = 20)
     {
+        if (color == null) { color = Color.white; }
         Vector2 firstPoint = Vector2.zero;
         Vector2 currentPoint;
         Vector2 prevPoint = Vector2.zero;
@@ -61,7 +65,7 @@ public static class Draw
             currentPoint = center + delta;
             if (angle != 0f)
             {
-                Debug.DrawLine(currentPoint, prevPoint, color);
+                Debug.DrawLine(currentPoint, prevPoint, (Color)color);
             }
             else
             {
@@ -69,19 +73,20 @@ public static class Draw
             }
             prevPoint = currentPoint;
         }
-        Debug.DrawLine(prevPoint, firstPoint, color);
+        Debug.DrawLine(prevPoint, firstPoint, (Color)color);
     }
 
-    public static void Arc(Vector2 center, float radius, Vector2 startDirection, Vector2 endDirection, Color color, bool clockwise = true, int resolution = 20)
+    public static void Arc(Vector2 center, float radius, Vector2 startDirection, Vector2 endDirection, Color? color = null, bool clockwise = true, int resolution = 20)
     {
-
+        if (color == null) { color = Color.white; }
         float startAngle = Vector2.SignedAngle(Vector2.right, startDirection);
         float endAngle = Vector2.SignedAngle(Vector2.right, endDirection);
         Arc(center, radius, startAngle, endAngle, color, clockwise, resolution);
     }
 
-    public static void Arc(Vector2 center, float radius, float startAngle, float endAngle, Color color, bool clockwise = true, int resolution = 20)
+    public static void Arc(Vector2 center, float radius, float startAngle, float endAngle, Color? color = null, bool clockwise = true, int resolution = 20)
     {
+        if (color == null) { color = Color.white; }
         Vector2 currentPoint;
         Vector2 prevPoint = Vector2.zero;
 
@@ -106,34 +111,42 @@ public static class Draw
             currentPoint = center + delta;
             if (angle != startAngle)
             {
-                Debug.DrawLine(currentPoint, prevPoint, color);
+                Debug.DrawLine(currentPoint, prevPoint, (Color)color);
             }
             prevPoint = currentPoint;
         }
-        //Debug.Log("Start: " + startAngle + " End: "+ endAngle + " Delta: "+ deltaAngle);
-        Debug.DrawLine(prevPoint, center + new Vector2(Mathf.Cos(endAngle), Mathf.Sin(endAngle)) * radius, color);
+        Debug.DrawLine(prevPoint, center + new Vector2(Mathf.Cos(endAngle), Mathf.Sin(endAngle)) * radius, (Color)color);
     }
 
-    public static void Arrow(Vector2 origin, Vector2 target, Color color, float size = 0.1f)
+    public static void ArrowHead(Vector2 origin, Vector2 target, Color? color = null, float size = 0.1f)
     {
+        if (color == null) { color = Color.cyan; }
         var tangent = (target - origin).normalized;
         var normal = new Vector2(-tangent.y, tangent.x);
-        //Debug.DrawLine(origin, target, color);
-        Debug.DrawLine(origin - normal * size, target, color);
-        Debug.DrawLine(origin + normal * size, target, color);
-        Debug.DrawLine(origin + normal * size, origin - normal * size, color);
+
+        Debug.DrawLine(origin - normal * size, target, (Color)color);
+        Debug.DrawLine(origin + normal * size, target, (Color)color);
+        Debug.DrawLine(origin + normal * size, origin - normal * size, (Color)color);
     }
 
-    public static void Vector(Vector2 origin, Vector2 target, Color color, float size = 0.1f, bool closedArrowHead = true)
+    public static void Vector(Vector2 origin, Vector2 target, Color? color = null, float size = 0.1f, bool closedArrowHead = true)
     {
+        if (color == null) { color = Color.cyan; }
         var tangent = (target - origin).normalized;
         var normal = new Vector2(-tangent.y, tangent.x);
-        Debug.DrawLine(origin, closedArrowHead ? target - tangent * size : target, color);
-        Debug.DrawLine(target, target - (tangent + normal) * size, color);
-        Debug.DrawLine(target, target - (tangent - normal) * size, color);
+        Debug.DrawLine(origin, closedArrowHead ? target - tangent * size : target, (Color)color);
+        Debug.DrawLine(target, target - (tangent + normal) * size, (Color)color);
+        Debug.DrawLine(target, target - (tangent - normal) * size, (Color)color);
         if (closedArrowHead)
         {
-            Debug.DrawLine(target - (tangent + normal) * size, target - (tangent - normal) * size, color);
+            Debug.DrawLine(target - (tangent + normal) * size, target - (tangent - normal) * size, (Color)color);
         }
+    }
+
+    public static void Cross(Vector2 position, Color? color = null, float size = 0.1f)
+    {
+        if (color == null) { color = Color.red; }
+        Debug.DrawLine(position + Vector2.one * size, position - Vector2.one * size, (Color)color);
+        Debug.DrawLine(position + Vector2.one.LeftPerpendicular() * size, position - Vector2.one.LeftPerpendicular() * size, (Color)color);
     }
 }
