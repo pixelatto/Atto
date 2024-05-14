f62ff0e5813e5dd10552511bc0e69dd951fde2f0
JaviCepa <javi@pixelatto.com>
2024-05-14 09:35:35 +0200

    Improved fireplace

--- Runtime/Scripts/Entities/Gadgets/Fireplace.cs
+++ Runtime/Scripts/Entities/Gadgets/Fireplace.cs
@@ -1,9 +1,12 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine;
 
 public class Fireplace : Gadget
 {
+    public float remainingFuel = 100;
+
     public CellularSensor cellularSensor { get { if (cellularSensor_ == null) { cellularSensor_ = GetComponent<CellularSensor>(); }; return cellularSensor_; } }
     private CellularSensor cellularSensor_;
 
@@ -13,32 +16,42 @@ public class Fireplace : Gadget
     public CellularSpawner cellularSpawner { get { if (cellularSpawner_ == null) { cellularSpawner_ = GetComponentInChildren<CellularSpawner>(); }; return cellularSpawner_; } }
     private CellularSpawner cellularSpawner_;
 
-    void Update()
+    public override void On_State()
     {
-        switch (state)
+        remainingFuel -= Time.deltaTime;
+        cellularSpawner.gameObject.SetActive(true);
+        pixelLight.gameObject.SetActive(true);
+        if (cellularSensor.IsFilledWith((cell => (cell.IsLiquid() || cell.IsSolid()) && !cell.IsHotterThan(300))))
         {
-            case GadgetState.On:
-                cellularSpawner.gameObject.SetActive(true);
-                pixelLight.gameObject.SetActive(true);
-                if (cellularSensor.IsFilledWith((cell => (cell.IsLiquid() || cell.IsSolid()) && !cell.IsHotterThan(300))))
-                {
-                    ChangeState(GadgetState.Off);
-                }
-                break;
-            case GadgetState.Off:
-                cellularSpawner.gameObject.SetActive(false);
-                pixelLight.gameObject.SetActive(false);
-                if (cellularSensor.ContainsAny((cell => cell.IsHotterThan(300))))
-                {
-                    ChangeState(GadgetState.On);
-                }
-                break;
-            case GadgetState.Out:
-                break;
-            default:
-                break;
+            ChangeState(GadgetState.Off);
         }
+        if (remainingFuel <= 0)
+        {
+            remainingFuel = 0;
+            ChangeState(GadgetState.Out);
+        }
+    }
 
+    public override void Off_State()
+    {
+        base.Off_State();
+        cellularSpawner.gameObject.SetActive(false);
+        pixelLight.gameObject.SetActive(false);
+        if (cellularSensor.ContainsAny((cell => cell.IsHotterThan(300))))
+        {
+            ChangeState(GadgetState.On);
+        }
+    }
+
+    public override void Out_State()
+    {
+        base.Out_State();
+        cellularSpawner.gameObject.SetActive(false);
+        pixelLight.gameObject.SetActive(false);
+        if (remainingFuel > 0)
+        {
+            ChangeState(GadgetState.Off);
+        }
     }
 
 }
--- Runtime/Scripts/Entities/Gadgets/Gadget.cs
+++ Runtime/Scripts/Entities/Gadgets/Gadget.cs
@@ -7,10 +7,42 @@ public abstract class Gadget : MonoBehaviour
     public Animator animator { get { if (animator_ == null) { animator_ = GetComponentInChildren<Animator>(); }; return animator_; } }
     private Animator animator_;
 
+    GadgetState checkState;
+
+    protected void Update()
+    {
+        if (state != checkState)
+        {
+            ChangeState(state);
+        }
+
+        switch (state)
+        {
+            case GadgetState.On:  On_State();  break;
+            case GadgetState.Off: Off_State(); break;
+            case GadgetState.Out: Out_State(); break;
+        }
+    }
+
     public void ChangeState(GadgetState state)
     {
         this.state = state;
+        checkState = state;
         animator.Play(state.ToString().ToLowerInvariant());
     }
 
+    public virtual void On_State()
+    {
+
+    }
+
+    public virtual void Off_State()
+    {
+
+    }
+
+    public virtual void Out_State()
+    {
+
+    }
 }
