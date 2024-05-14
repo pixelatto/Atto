1b4f3518ac4d4e987a15d29900e9bcc9305c2ddc
JaviCepa <javi@pixelatto.com>
2024-05-14 20:24:53 +0200

    Add reaction definitions and data access

---
+++ Runtime/Systems/Automatas/Scripts/Cells/CellMaterialReaction.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+[System.Serializable]
+public class CellMaterialReaction
+{
+    public string reactionName;
+    public CellMaterial reactorA;
+    public CellMaterial reactorB;
+    public CellMaterial productA;
+    public CellMaterial productB;
+    [Range(0, 1)] public float chance = 1;
+}
--- Runtime/Systems/Automatas/Scripts/Cells/CellularMaterials.cs
+++ Runtime/Systems/Automatas/Scripts/Cells/CellularMaterials.cs
@@ -10,26 +10,58 @@ public class CellularMaterials : ScriptableObject
     public List<CellMaterialProperties> materials;
     public List<CellMaterialReaction> reactions;
 
-    static Dictionary<CellMaterial, CellMaterialProperties> lookupDictionary = new Dictionary<CellMaterial, CellMaterialProperties>();
+    static Dictionary<CellMaterial, CellMaterialProperties> materialLookupDictionary = new Dictionary<CellMaterial, CellMaterialProperties>();
+    static Dictionary<CellMaterial, Dictionary<CellMaterial, CellMaterialReaction>> reactionLookupDictionary = new Dictionary<CellMaterial, Dictionary<CellMaterial, CellMaterialReaction>>();
+
+    private void OnEnable()
+    {
+        InitializeDictionaries();
+    }
+
+    private void InitializeDictionaries()
+    {
+        materialLookupDictionary.Clear();
+        reactionLookupDictionary.Clear();
+
+        foreach (var material in materials)
+        {
+            materialLookupDictionary[material.cellMaterial] = material;
+        }
+
+        foreach (var reaction in reactions)
+        {
+            if (!reactionLookupDictionary.ContainsKey(reaction.reactorA))
+            {
+                reactionLookupDictionary[reaction.reactorA] = new Dictionary<CellMaterial, CellMaterialReaction>();
+            }
+            if (!reactionLookupDictionary.ContainsKey(reaction.reactorB))
+            {
+                reactionLookupDictionary[reaction.reactorB] = new Dictionary<CellMaterial, CellMaterialReaction>();
+            }
+
+            reactionLookupDictionary[reaction.reactorA][reaction.reactorB] = reaction;
+            reactionLookupDictionary[reaction.reactorB][reaction.reactorA] = reaction; // Ensure both orders are stored
+        }
+    }
 
     public CellMaterialProperties FindMaterial(CellMaterial cellMaterial)
     {
-        if (!lookupDictionary.ContainsKey(cellMaterial))
-        {
-            lookupDictionary.Add(cellMaterial, instance.materials.Find(x => x.cellMaterial == cellMaterial));
-        }
-        
-        return lookupDictionary[cellMaterial];
+        return materialLookupDictionary[cellMaterial];
     }
-}
 
-[System.Serializable]
-public class CellMaterialReaction
-{
-    public string reactionName;
-    public CellMaterial reactorA;
-    public CellMaterial reactorB;
-    public CellMaterial productA;
-    public CellMaterial productB;
-    [Range(0, 1)] public float chance = 1;
+    public CellMaterialReaction FindReaction(CellMaterial reactorA, CellMaterial reactorB)
+    {
+        if (reactionLookupDictionary.ContainsKey(reactorA) && reactionLookupDictionary[reactorA].ContainsKey(reactorB))
+        {
+            return reactionLookupDictionary[reactorA][reactorB];
+        }
+        else if (reactionLookupDictionary.ContainsKey(reactorB) && reactionLookupDictionary[reactorB].ContainsKey(reactorA))
+        {
+            return reactionLookupDictionary[reactorB][reactorA];
+        }
+        else
+        {
+            return null; // No reaction found
+        }
+    }
 }
