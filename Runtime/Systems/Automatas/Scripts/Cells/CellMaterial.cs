3a8cbb013a5f3e1df259c8ee573852458063dd78
JaviCepa <javi@pixelatto.com>
2024-05-17 20:36:10 +0200

    Thermal adjustments

--- Runtime/Systems/Automatas/Scripts/Cells/CellMaterial.cs
+++ Runtime/Systems/Automatas/Scripts/Cells/CellMaterial.cs
@@ -4,7 +4,7 @@
     Stone = 1,
     Dirt = 2,
     Water = 3,
-    Lava = 4,
+    Magma = 4,
     Fire = 5,
     Acid = 6,
     Snow = 7,
@@ -13,6 +13,6 @@
     Steam = 10,
     Ice = 11,
     Metal = 12,
-    Smelt = 13
+    Lava = 13
 }
 
