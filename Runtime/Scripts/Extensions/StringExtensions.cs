bf6be02e01aee3165fff7a74d6da777312b923bd
JaviCepa <javi@pixelatto.com>
2024-05-22 11:33:56 +0200

    Some string extensions

---
+++ Runtime/Scripts/Extensions/StringExtensions.cs
@@ -0,0 +1,9 @@
+﻿using System.Text.RegularExpressions;
+
+public static class StringExtensions
+{
+    public static string AddSpacesToCamelCase(string input)
+    {
+        return Regex.Replace(input, "([a-z])([A-Z])", "$1 $2");
+    }
+} 
